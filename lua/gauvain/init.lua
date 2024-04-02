aa39ffeb42151561d11752ba6f9b9c17a426a6af
Gauvain01 <steijejodh@gmail.com>
2024-04-02 11:27:42 +0200

    first commit
    clear
    :q
    q
    '';;
    ;

---
+++ lua/gauvain/init.lua
@@ -0,0 +1,9 @@
+require("gauvain.remap")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set number relativenumber")
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>',{})
+vim.cmd("highlight normal ctermbg=none")
+
+
+
