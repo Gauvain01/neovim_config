4e63a07184c82781001e05fefab75c017c0765a0
Gauvain01 <steijejodh@gmail.com>
2024-04-02 17:42:00 +0200

    + trouble

--- lua/gauvain/plugins/harpoon.lua
+++ lua/gauvain/plugins/harpoon.lua
@@ -7,15 +7,11 @@ return {
 
 		vim.keymap.set("n", "<leader>a", mark.add_file)
 		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-		vim.keymap.set("n", "<leader>u",function() ui.nav_file(1)  end)
-		vim.keymap.set("n", "<leader>i", function() ui.nav_file(2) end)
-		vim.keymap.set("n", "<leader>o", function() ui.nav_file(3) end)
-		vim.keymap.set("n", "<leader>p", function() ui.nav_file(4) end)
+		vim.keymap.set("n", "<leader>h",function() ui.nav_file(1)  end)
+		vim.keymap.set("n", "<leader>j", function() ui.nav_file(2) end)
+		vim.keymap.set("n", "<leader>k", function() ui.nav_file(3) end)
+		vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
 		vim.keymap.set("n", "<C-o>", function() term.gotoTerminal(1)end)
-	
-		
-		
-
 
 	end
 }
